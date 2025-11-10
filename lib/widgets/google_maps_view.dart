38887b00d85d8cc24634c8ca07ab9b8c0a6fd4c5
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-11-10 21:03:15 +0200

    intialize google maps with flutter to IOS & ANDROID

--- lib/widgets/google_maps_view.dart
+++ lib/widgets/google_maps_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapsView extends StatefulWidget {
   const GoogleMapsView({super.key});
@@ -8,8 +9,19 @@ class GoogleMapsView extends StatefulWidget {
 }
 
 class _GoogleMapsViewState extends State<GoogleMapsView> {
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    initialCameraPosition = CameraPosition(target: LatLng(0, 0));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GoogleMap(
+      initialCameraPosition: initialCameraPosition,
+      zoomControlsEnabled: false,
+    );
   }
 }
