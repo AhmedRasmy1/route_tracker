63be945f5ee30197800b0e23095e90b9d6ddbadd
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-11-10 21:14:18 +0200

    feat: add location service implementation and update dependencies

---
+++ lib/utils/location_service.dart
@@ -0,0 +1,31 @@
+import 'package:location/location.dart';
+
+class LocationService {
+  Location location = Location();
+  Future<bool> checkAndRequestLocationService() async {
+    var isEnabledService = await location.serviceEnabled();
+    if (!isEnabledService) {
+      await location.requestService();
+      if (!isEnabledService) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<bool> checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.deniedForever) {
+      return false;
+    }
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      return permissionStatus == PermissionStatus.granted;
+    }
+    return true;
+  }
+
+  void getRealTimeLocationData(void Function(LocationData)? onData) {
+    location.onLocationChanged.listen(onData);
+  }
+}
